dcbdf3932004b8b0663649137638f3e1ef3abec6
Ellisse Perez <143231305+EllissePerez@users.noreply.github.com>
2023-09-08 16:32:33 -0400

    Added JavaScript and HTML file to repo

---
+++ JavaScript Source.js
@@ -0,0 +1,10 @@
+
+var firstName = "Ellisse";
+var lastName = "Perez";
+var age = 24;
+
+alert("My name is " + firstName + " " + lastName);
+
+alert("I am " + age + " years old");
+
+console.log("I'm having a great time learning about HTML and JavaScript!");
